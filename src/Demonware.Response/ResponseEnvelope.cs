e7f2e334f42e844dd1d895e0314fb43e7fdbabe9
Eric Freeman <clownhunter@gmail.com>
2015-06-23 22:43:14 -0600

    Almost forgot to add json property values to the response envelope.

--- src/Demonware.Response/ResponseEnvelope.cs
+++ src/Demonware.Response/ResponseEnvelope.cs
@@ -1,9 +1,16 @@
-﻿namespace Demonware.Response
+﻿using Newtonsoft.Json;
+
+namespace Demonware.Response
 {
     public class ResponseEnvelope<T>
     {
+        [JsonProperty(PropertyName = "message")]
         public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "completed")]
         public bool Completed{ get; set; }
+
+        [JsonProperty(PropertyName = "result")]
         public T Result { get; set; }
     }
 }
