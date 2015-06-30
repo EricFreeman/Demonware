d05804e50f5587254f7a31e14d69ab48e5901765
Eric Freeman <clownhunter@gmail.com>
2015-06-29 22:49:41 -0600

    Added a controller for dynamic calls.  This makes it so we don't have to keep demonware up to date with what's in demandware.  We can call any pipeline we want with any parameters at this point.

---
+++ src/Demonware/Controllers/DemonwareController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Results;
+using Demonware.Models;
+using Demonware.Request;
+using Demonware.Response;
+using RestSharp;
+
+namespace Demonware.Controllers
+{
+    [EnableCors(origins: "http://prototype.1800contacts.com", headers: "*", methods: "*")]
+    public class DemonwareController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<object> Post(RequestModel request)
+        {
+            if (request.Parameters == null)
+            {
+                request.Parameters = new RequestParameter[0];
+            }
+
+            var result = Hell.HailSatan<ResponseEnvelope<Product>>(request.Url, Method.POST, request.Parameters);
+            return Json(result.Data as object);
+        }
+    }
+}
---
+++ src/Demonware/Models/RequestModel.cs
@@ -0,0 +1,10 @@
+﻿using Demonware.Request;
+
+namespace Demonware.Models
+{
+    public class RequestModel
+    {
+        public string Url { get; set; }
+        public RequestParameter[] Parameters { get; set; }
+    }
+}
