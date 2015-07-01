d16cd003e12c0b7a0f4b147f9b68f71b8b6c0629
Eric Freeman <clownhunter@gmail.com>
2015-06-30 22:53:16 -0600

    Dropped need to /post/ when hitting demonware.

--- src/Demonware/App_Start/WebApiConfig.cs
+++ src/Demonware/App_Start/WebApiConfig.cs
@@ -11,6 +11,12 @@ namespace Demonware
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ExplicitApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
